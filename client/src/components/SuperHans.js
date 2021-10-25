60c2c1d730874f9e6c636d7226ea159d4cb7b334
jckrft <jckrft@gmail.com>
2021-10-25 11:28:24 -0400

    superhans

--- client/src/components/SuperHans.js
+++ client/src/components/SuperHans.js
@@ -1,1 +1,9 @@
-// export default SuperHans;
+function SuperHans({ data, toggleFetch, setToggleFetch }) {
+  
+  const superHansData = data.filter(quote => quote.fields.character === 'super hans')
+
+  return (
+    
+  )
+
+export default SuperHans;
