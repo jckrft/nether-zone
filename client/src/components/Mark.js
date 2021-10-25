f8bc2a05512abfe49bc1f52b3d097cee5e15cfca
jckrft <jckrft@gmail.com>
2021-10-25 14:36:08 -0400

    mark function

--- client/src/components/Mark.js
+++ client/src/components/Mark.js
@@ -2,4 +2,12 @@ import Comments from './Comments.js'
 
 import Carousel from 'react-bootstrap/Carousel'
 
+function Mark({ data, toggleFetch, setToggleFetch }) {
+  
+  const markData = data.filter(quote => quote.fields.character === 'mark')
+  
+  return(
+
+  )
+
 export default Mark;
